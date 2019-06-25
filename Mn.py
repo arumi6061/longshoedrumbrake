7e94f0e3800eef43fdc2ac3e47a502d09a15d9d0
Arumi6061 <45915811+arumi6061@users.noreply.github.com>
2019-06-25 16:32:40 +0800

    Update Mn.py

--- Mn.py
+++ Mn.py
@@ -1,6 +1,6 @@
 #Normal moment exerted on long shoe drum brake
 #Change variable below to find Mn
-omega=0.07
+w=0.07
 r=0.18
 c=0.249
 Pmax=780e3
@@ -9,5 +9,5 @@ theta1=math.pi/6
 theta2=math.pi/2
 psi=1.047 # 20 + 40 degree from diagram
 
-Mn=(omega*r*c*Pmax/(4*sinTheta))*(2*psi-math.sin(2*theta2)+math.sin(2*theta1))
+Mn=(w*r*c*Pmax/(4*sinTheta))*(2*psi-math.sin(2*theta2)+math.sin(2*theta1))
 Mn
